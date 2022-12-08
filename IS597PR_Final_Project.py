9557f248c93e92925eab00e0dd1be3fbf2351356
Noel Thomas <noel.thomas27@gmail.com>
2022-12-08 14:31:52 -0600

    Made some changes to the cleanup functions.

--- IS597PR_Final_Project.py
+++ IS597PR_Final_Project.py
@@ -56,59 +56,60 @@ def fixcolumns(df:pd.DataFrame, column_name:str) -> pd.DataFrame:
     """
 
     if column_name == 'Type of Breach':
-        for x in df['Type of Breach']:
+        for x in df[column_name]:
             if 'Hacking/IT Incident' in x:
-                df.loc[df['Type of Breach'] == x, 'Breach Type'] = 'Hacking/IT Incident'
+                df.loc[df[column_name] == x, 'Breach Type'] = 'Hacking/IT Incident'
                 continue
             if 'Improper Disposal' in x:
-                df.loc[df['Type of Breach'] == x, 'Breach Type'] = 'Improper Disposal'
+                df.loc[df[column_name] == x, 'Breach Type'] = 'Improper Disposal'
                 continue
             if 'Loss' in x:
-                df.loc[df['Type of Breach'] == x, 'Breach Type'] = 'Loss'
+                df.loc[df[column_name] == x, 'Breach Type'] = 'Loss'
                 continue
             if 'Unauthorized Access/Disclosure' in x:
-                df.loc[df['Type of Breach'] == x, 'Breach Type'] = 'Unauthorized Access/Disclosure'
+                df.loc[df[column_name] == x, 'Breach Type'] = 'Unauthorized Access/Disclosure'
                 continue
             if 'Theft' in x:
-                df.loc[df['Type of Breach'] == x, 'Breach Type'] = 'Theft'
+                df.loc[df[column_name] == x, 'Breach Type'] = 'Theft'
                 continue
             if 'Unknown' in x or 'Other' in x:
-                df.loc[df['Type of Breach'] == x, 'Breach Type'] = 'Other/Unkown'
+                df.loc[df[column_name] == x, 'Breach Type'] = 'Other/Unkown'
                 continue
-        df.drop(['Type of Breach'], axis=1, inplace=True)
+        df.drop([column_name], axis=1, inplace=True)
+        df = df.rename(columns={'Breach Type': column_name})
         return df
 
     if column_name == 'Location of Breached Information':
-        for x in df['Location of Breached Information']:
+        for x in df[column_name]:
             if 'Desktop Computer' in x:
-                df.loc[df['Location of Breached Information'] == x, 'Location'] = 'Desktop Computers'
+                df.loc[df[column_name] == x, 'Location'] = 'Desktop Computers'
                 continue
             if 'Electronic Medical Record' in x:
-                df.loc[df['Location of Breached Information'] == x, 'Location'] = 'Electronic health records'
+                df.loc[df[column_name] == x, 'Location'] = 'Electronic health records'
                 continue
             if 'Email' in x:
-                df.loc[df['Location of Breached Information'] == x, 'Location'] = 'E-mail'
+                df.loc[df[column_name] == x, 'Location'] = 'E-mail'
                 continue
             if 'Laptop' in x:
-                df.loc[df['Location of Breached Information'] == x, 'Location'] = 'Laptops'
+                df.loc[df[column_name] == x, 'Location'] = 'Laptops'
                 continue
             if 'Network Server' in x:
-                df.loc[df['Location of Breached Information'] == x, 'Location'] = 'Network Servers'
+                df.loc[df[column_name] == x, 'Location'] = 'Network Servers'
                 continue
             if 'Other' in x:
-                df.loc[df['Location of Breached Information'] == x, 'Location'] = 'Others'
+                df.loc[df[column_name] == x, 'Location'] = 'Others'
             if 'Paper' in x:
-                df.loc[df['Location of Breached Information'] == x, 'Location'] = 'Paper'
+                df.loc[df[column_name] == x, 'Location'] = 'Paper'
                 continue
-        df.drop(['Location of Breached Information'], axis=1, inplace=True)
+        df.drop([column_name], axis=1, inplace=True)
+        df = df.rename(columns={'Location': 'Location of breach'})
         return df
 
 
 if __name__ == '__main__':
     d_parser = lambda x: pd.to_datetime(x, errors='coerce')
 
-    df = pd.read_csv('breach_report.csv', encoding='latin1',
-                     dtype={'Type of Breach': 'string'},
+    df = pd.read_csv('breach_report.csv', encoding='latin1', dtype={'Type of Breach': 'string'},
                      parse_dates=['Breach Submission Date'], date_parser=d_parser)
 
     df1 = adjust_time_limits(df, '2013-09-22')
